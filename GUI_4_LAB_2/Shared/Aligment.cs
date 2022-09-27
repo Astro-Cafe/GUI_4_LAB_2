d988bd60264515cbc22b38bbc0663d04e3366c9b
Kecskeméti.Levente <kiskecsoka@gmail.com>
2022-09-27 18:15:50 +0200

    Enum moved to Shared

---
+++ GUI_4_LAB_2/Shared/Aligment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_4_LAB_2.Shared
+{
+    public enum Aligment
+    {
+        evil, neutral, good
+    }
+}
